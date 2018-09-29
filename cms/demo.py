7ca156a320c74400fea7a62219dd0dff1a726c67
boy-hack <34109680@qq.com>
2018-09-29 15:55:19 +0800

    minnor typo

--- cms/demo.py
+++ cms/demo.py
@@ -7,7 +7,7 @@ def assign(service, arg):
         return True, arg
 
 
-def poc(arg):
+def poc(arg, **kwargs):
     result = {
         "name": "Demo插件",  # 插件名称
         "content": "如果这个插件能显示出来，就说明w12scan框架测试成功了",  # 插件返回内容详情，会造成什么后果。
